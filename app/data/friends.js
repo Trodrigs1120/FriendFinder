1c346c7f41d0b3747b123e4a8ea667318df0a5c2
trodrigs1120 <trodrigs@yahoo.com>
2018-02-27 18:29:52 -0500

    updates all over the place

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,16 +7,31 @@
 var SurveyArray = [
   {
     friendName: "Lauren",
-    friendEmail: "LMudd@example.com",
-    friendID: "LMoney",
-    friendSurvey: [1,2,3,4,5,5,4,3,2,1,5]
+    friendphoto: "url",
+    scores: [1,2,3,5,5,5,4,3,2,1,5]
   },
   {
     friendName: "Ahmed",
-    friendEmail: "ahmed@example.com",
-    friendID: "afhaque89",
-    friendSurvey: [1,2,3,4,5,5,4,3,2,1,5]
+    friendphoto: "url",
+    scores: [1,2,3,4,5,5,4,3,2,1,5]
   }
+  ,
+  {
+    friendName: "Larry",
+    friendphoto: "url",
+    scores: [5,4,1,3,2,5,3,3,5,1,2]
+  },
+  {
+    friendName: "Jordyn",
+    friendphoto: "url",
+    scores: [1,2,3,2,5,5,4,3,2,1,1]
+  },
+  {
+    friendName: "Fred",
+    friendphoto: "url",
+    scores: [1,2,1,5,1,5,3,3,2,1,3]
+  }
+
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var surveyData = require("../data/friends");
+var matchData = require("../data/friends");
 // var waitListData = require("../data/waitinglistData");
 
 
